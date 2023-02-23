c287e579ad2d3bee5ecb72ba7f959021aa7ae258
Vlad Makhnenko <vlmakh@gmail.com>
2023-02-23 08:13:10 +0200

    update logo size

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 import { ReactComponent as LogoSvg } from 'images/vm_logo.svg';
 
 export const Logo = () => {
-  return <LogoSvg width="100" height="72" />;
+  return <LogoSvg width="100" height="64" />;
 };
